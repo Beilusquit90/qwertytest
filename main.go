1c27265e491c7b6e30db1cdf071b50e54b4cce6c
go_pnik <beilusquit90@gmail.com>
2024-09-28 21:45:04 +0300

    q

--- main.go
+++ main.go
@@ -22,6 +22,10 @@ func main() {
 	// r.Use(LoggerMiddleware())
 	// клоака и перехват эндпоинтов
 	// r.GET("/index.html", cloak.EditedIndex)
+
+	// [GIN] 2024/09/28 - 21:42:40 | 404 |     314.403µs |     185.44.70.0 | GET      "/assets-ucp/EhkO/i9536e69d084378a3147301fc49f47507/_r192x192_png"
+	// [GIN] 2024/09/28 - 21:42:40 | 404 |     226.761µs |     185.44.70.0 | GET      "/assets-ucp/EhkO/i9536e69d084378a3147301fc49f47507/_r48x48_png"
+	// [GIN] 2024/09/28 - 21:42:40 | 404 |     198.058µs |     185.44.70.0 | GET      "/assets-ucp/EhkO/i9536e69d084378a3147301fc49f47507/_r96x96_png"
 	r.StaticFS("/", gin.Dir("./999", false))
 	err := r.Run(":80")
 	if err != nil {
