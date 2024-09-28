62bb31cc42048a06ffb534e70047c5501ad7a446
go_pnik <beilusquit90@gmail.com>
2024-09-29 00:51:18 +0300

    q

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	// [GIN] 2024/09/28 - 21:42:40 | 404 |     314.403µs |     185.44.70.0 | GET      "/assets-ucp/EhkO/i9536e69d084378a3147301fc49f47507/_r192x192_png"
 	// [GIN] 2024/09/28 - 21:42:40 | 404 |     226.761µs |     185.44.70.0 | GET      "/assets-ucp/EhkO/i9536e69d084378a3147301fc49f47507/_r48x48_png"
 	// [GIN] 2024/09/28 - 21:42:40 | 404 |     198.058µs |     185.44.70.0 | GET      "/assets-ucp/EhkO/i9536e69d084378a3147301fc49f47507/_r96x96_png"
-	r.StaticFS("/", gin.Dir("./pwa-machine-app-template", false))
+	r.StaticFS("/", gin.Dir("./pwa-machine-app-template/dist", false))
 	err := r.Run(":80")
 	if err != nil {
 		panic(err)
